491fe8801c6bf68d604c9ec3458814d94798a814
luked123 <luked@uoregon.edu>
2017-03-12 21:30:47 -0700

    Think I finally finished it aint pretty though lol

--- lab4/src/RBT.java
+++ lab4/src/RBT.java
@@ -2,26 +2,37 @@ public class RBT<E extends Comparable<E>> {
     private Node<E> root; 
     
     public RBT(){
+    // Constructor
+   
         root = null;
     }
 
     public Node<E> getRoot(){
+    // Returns root of tree
+    	
         return root;
     }
 
     public void insert(E data){
+    // Inserts node in correct place of RBT tree
+    // Calls fixInsert when finished 
+    	
 		Node<E> node = new Node<E>(data);
-		Node<E> nil  = new Node<E>(null);
+		Node<E> nilL  = new Node<E>(null);
+		Node<E> nilR  = new Node<E>(null);
 		
-		nil.setColor('b');
-		nil.setParent(node);
+		nilL.setColor('b');
+		nilR.setColor('b');
+		nilL.setParent(node);
+		nilR.setParent(node);
 		
-		node.setLeftChild(nil);
-		node.setRightChild(nil);
+		node.setLeftChild(nilL);
+		node.setRightChild(nilR);
 		node.setColor('r');
 		
-		// if tree is empty make node into root
 		if(this.root == null){
+		// If tree is empty make node root
+
 			this.root = node; 
 		}
 		else{
@@ -47,8 +58,9 @@ public class RBT<E extends Comparable<E>> {
     }
 
     public Node<E> search(E data){
-        // Return the node that corresponds with the given data
-        // finds node matching data in BST	
+    // Return the node that corresponds with the given data
+    // finds node matching data in RBT
+    	
     	Node<E> node = new Node<E>(data); 
     	Node<E> cur = this.root;
     	
@@ -62,39 +74,55 @@ public class RBT<E extends Comparable<E>> {
     	}
     	
     	if(cur.getData() == null){
+    	// No node was found
     		return null; 
     	}
     	
     	return cur; 
-        // Note: No need to worry about duplicate values added to the tree
     }
 
+    
     public void delete(E data){
-    	// Preform a regular delete
-    	Node<E> node = search(data); 
-    	Node<E> nil = new Node<E>(null); 
-    	nil.setColor('b');
-    	nil.setParent(node);
+    // Performs a regular delete and depending on case calls fixDelete
+    	Node<E> node = search(data);
+    	
+    	Node<E> nil; 
+    	Node<E> nilL = new Node<E>(null); 
+    	Node<E> nilR = new Node<E>(null); 
+    	nilL.setColor('b');
+    	nilR.setColor('b');
     	
     	if(node == null){
+    	// No node was found
     		return; 
     	}
     	
-    	// node has no children
+    
     	if(node.getLeftChild().getData() == null && node.getRightChild().getData() == null){
+    	// Node has no children 	
     		if(node.getParent() == null){
-    			this.root = null; 
+    			this.root = null;
+    			return; 
     		}
     		else if(isLeft(node)){
-    			node.getParent().setLeftChild(nil);
+    			nilL.setParent(node.getParent());
+    			node.getParent().setLeftChild(nilL);
+    			nil = nilL; 
     		}
     		else{
-    			node.getParent().setRightChild(nil);
+    			nilR.setParent(node.getParent());
+    			node.getParent().setRightChild(nilR);
+    			nil = nilR; 
     		}
+    		
+    		if(node.getColor() == 'b'){           // only perform if node was black
+    			fixDelete(nil);
+    		}
+   
     	}
     	
-    	//node has left child
     	else if(node.getLeftChild().getData() != null && node.getRightChild().getData() == null){
+    	// Node has left child
     		if(node.getParent() == null){
     			this.root = node.getLeftChild(); 
     		}
@@ -106,10 +134,13 @@ public class RBT<E extends Comparable<E>> {
     			node.getParent().setRightChild(node.getLeftChild());
     			node.getLeftChild().setParent(node.getParent()); 
     		}
+    		if(node.getColor() == 'b' ){                // only perform if node was black
+    			fixDelete(node.getLeftChild());
+    		}
     	}
     	
-    	// node has right child
     	else if(node.getLeftChild().getData() == null && node.getRightChild().getData() != null){
+    	// Node has right child
     		if(node.getParent() == null){
     			this.root = node.getRightChild();  
     		}
@@ -121,11 +152,16 @@ public class RBT<E extends Comparable<E>> {
     			node.getParent().setRightChild(node.getRightChild());
     			node.getRightChild().setParent(node.getParent());
     		}
+    		if(node.getColor() == 'b'){               // only perform if node was black
+    			fixDelete(node.getRightChild()); 
+    		}
     	}
     	
     	else{
+    	// Node has two children 	
     		Node<E> suc = findSuc(node); 
     		delete(suc.getData()); 
+    		suc.setColor(node.getColor());
     		
     		if(node.getParent() == null){
     			suc.setLeftChild(node.getLeftChild());
@@ -143,28 +179,34 @@ public class RBT<E extends Comparable<E>> {
     			node.getParent().setRightChild(suc);
     		}
     	}
-    // Check to make sure the tree remains an RBT tree
     }
-    
+    	
     
     public void traverse(String order, Node<E> top) {
-        // Preform a preorder traversal of the tree
+    // Traverses tree in indicated order 
+    	
     	if(top.getData() != null){
     		if(order == "preorder"){
-    			if(top.getColor() == 'r'){
-    				System.out.printf("%d:R ", top.getData());
-    			}
-    			else{
-    				System.out.printf("%d:b ", top.getData()); 
-    			}
+    			System.out.printf("%d ", top.getData());
     			traverse("preorder", top.getLeftChild());
     			traverse("preorder", top.getRightChild()); 
+    		}
+    		else if(order == "inorder"){
+    			traverse("inorder", top.getLeftChild());
+    			System.out.printf("%d ", top.getData());
+    			traverse("inorder", top.getRightChild());
+    		}
+    		else if(order =="postorder"){
+    			traverse("postorder", top.getLeftChild());
+				traverse("postorder", top.getRightChild());
+				System.out.printf("%d ", top.getData());
     		}
     	}
     }
 
 
     public void leftRotate(Node<E> y){
+    // Left rotate on node y
     	
     	Node<E> x = y.getParent(); 
     	Node<E> t1 = x.getLeftChild();
@@ -195,15 +237,10 @@ public class RBT<E extends Comparable<E>> {
     		this.root = roty; 
     	}
     	
-    	/*
-    	If x is the root of the tree to rotate with left child subtree T1 and right child y, 
-    	where T2 and T3 are the left and right children of y:
-			x becomes left child of y and T3 as its right child of y
-			T1 becomes left child of x and T2 becomes right child of x
-		*/
     }
 
     public void rightRotate(Node<E> x){
+    // Right rotates on node x
     	
     	Node<E> y = x.getParent();
     	Node<E> t1 = x.getLeftChild(); 
@@ -214,12 +251,14 @@ public class RBT<E extends Comparable<E>> {
     	Node<E> roty = new Node<E>(y.getData()); 
     	
     	roty.setParent(rotx);
+    	roty.setColor(y.getColor());
     	roty.setLeftChild(t2);
     	roty.setRightChild(t3);
     	t2.setParent(roty);
     	t3.setParent(roty);
     	
     	rotx.setParent(y.getParent());
+    	rotx.setColor(x.getColor());
     	rotx.setLeftChild(t1);
     	rotx.setRightChild(roty);
     	t1.setParent(rotx);
@@ -232,19 +271,13 @@ public class RBT<E extends Comparable<E>> {
     	}
     	else{
     		this.root = rotx; 
-    	}
-    	/*
-    	If y is the root of the tree to rotate with right child subtree T3 and left child x, 
-    	where T1 and T2 are the left and right children of x:
-			y becomes right child of x and T1 as its left child of x
-			T2 becomes left child of y and T3 becomes right child of y
-		*/
-		
+    	}	
     }
     
-
+    
     private boolean isRight(Node<E> n){
-    // Tests if node is right child. 	
+    // Tests if node is right child
+    	
     	if((n.getParent() != null) && (n == n.getParent().getRightChild())){
     		return true; 
     	}
@@ -252,23 +285,29 @@ public class RBT<E extends Comparable<E>> {
     }
     	
     private boolean isLeft(Node<E> n){
-    // Tests if node is left child.
+    // Tests if node is left child
+    	
     	if((n.getParent() != null) && (n == n.getParent().getLeftChild())){
     		return true; 
     	}
     	return false; 
     }
     
+    
     private Node<E> findSuc(Node<E> node){
-    	Node<E> suc = node.getRightChild(); 
-    	while(suc.getLeftChild().getData() != null){
-    		suc = suc.getLeftChild(); 
+    // Return the successor of the node
+    	
+    	node = node.getRightChild(); 
+    	while(node.getLeftChild().getData() != null){
+    		node = node.getLeftChild(); 
     	}
-    	return suc; 
+    	return node; 
     }
     
     
     private Node<E> getUncle(Node<E> n){
+    // Returns the Uncle of the node
+    	
     	if(isRight(n.getParent())){
     		return n.getParent().getParent().getLeftChild();
     	}
@@ -280,33 +319,43 @@ public class RBT<E extends Comparable<E>> {
     	}
     }
     
+    
     private Node<E> getGrandParent(Node<E> node){
+    // Returns the grandparent of the node
+    	
     	return node.getParent().getParent(); 
     }
     
     
-    private Node getSibling(Node<E> n){
+    private Node<E> getSibling(Node<E> n){
+    // Returns the sibling of the node
+    	
     	if(isRight(n)){
     		return n.getParent().getLeftChild();
     	}
     	else if(isLeft(n)){
-    		return n.getRightChild(); 
+    		return n.getParent().getRightChild(); 
     	}
     	else{
     		return null; 
     	}
     }
     
+    
     private void fixInsert(Node<E> node){
-    	//root case
+    // Fixes the red black tree at node during insertion
+    // Refers to cases found in text book
+    	
     	if(node == this.root){
     		node.setColor('b');
     	}
     	else{
     		if(node.getParent().getColor() == 'b'){
+    		// Do nothing if parent is black	
     			return; 
     		}
     		else{
+    		// Uncle is red case 
     			Node<E> uncle = getUncle(node);
     			Node<E> grandParent = getGrandParent(node); 
     			if(uncle.getColor() == 'r'){
@@ -316,8 +365,9 @@ public class RBT<E extends Comparable<E>> {
     				fixInsert(grandParent); 
     			}
     			else{
-    				// parent is left child 
+    			// Uncle is black case
     				if(isLeft(node.getParent())){
+    				// Parent is left child 
     					if(isRight(node)){
     						leftRotate(node);
     						rightRotate(node);
@@ -331,6 +381,7 @@ public class RBT<E extends Comparable<E>> {
     					}
     				}
     				else{
+    				// Parent is right child
     					if(isLeft(node)){
     						rightRotate(node);
     						leftRotate(node); 
@@ -346,11 +397,108 @@ public class RBT<E extends Comparable<E>> {
     			}
     		}
     	}
-    	
-    	
-    	
     }
-    // HINT: You may want to create extra methods such as fixDelete or fixInsert
+    
+    
+	private void fixDelete(Node<E> node){
+		// Fixes the Red black tree at node during deletion
+		// All cases differ from the book and refer to this video "https://www.youtube.com/watch?v=CTvfzU_uNKE"
+		// Implemented cases by myself
+
+		if(node.getColor() == 'r'){
+			if((node.getLeftChild().getData() == null) && (node.getRightChild().getData() == null)){
+				node.setColor('b');
+			}
+		}
+		else{
+			node.setColor('B');    // refers to double black node
+			Node<E> sibling = getSibling(node); 
+			if(node == this.root){   
+			// Case 1    
+				node.setColor('b');
+			}
+			if(isLeft(node)){
+			// Node is the left child
+				if((node.getParent().getColor() == 'b') && (sibling.getColor() == 'r') && (sibling.getLeftChild().getColor() == 'b') && (sibling.getRightChild().getColor() == 'b')){
+				// Case 2	
+					node.getParent().setColor('r');
+					sibling.setColor('b');
+					leftRotate(sibling); 
+					fixDelete(node); 
+				}
+				else if((node.getParent().getColor() == 'b') && (sibling.getColor() == 'b') && (sibling.getLeftChild().getColor() == 'b') && (sibling.getRightChild().getColor() == 'b')){
+				// Case 3	
+					sibling.setColor('r');
+					node.setColor('b');
+					fixDelete(node.getParent()); 
+				}
+				else if((node.getParent().getColor() == 'r') && (sibling.getColor() == 'b') && (sibling.getLeftChild().getColor() == 'b') && (sibling.getRightChild().getColor() == 'b')){
+				// Case 4	
+					node.getParent().setColor('b');
+					sibling.setColor('r');
+					node.setColor('b');
+				}
+				else if((sibling.getColor() == 'b') && (sibling.getRightChild().getColor() == 'b') && (sibling.getLeftChild().getColor() == 'r')){
+				// Case 5	
+					sibling.setColor('r');
+					sibling.getLeftChild().setColor('b');
+					rightRotate(sibling.getLeftChild());
+					fixDelete(node); 
+				}
+				else if((sibling.getColor() =='b') && (sibling.getRightChild().getColor() == 'r')){
+				// Case 6	
+					sibling.setColor(node.getParent().getColor());
+					node.getParent().setColor('b');
+					sibling.getRightChild().setColor('b');
+					node.setColor('b');
+					leftRotate(sibling); 
+				}
+				else{
+				// Just in case for some reason it does not go into any cases, was used for testing	
+				}
+			}
+			else{
+			// Node is right child	
+				if((node.getParent().getColor() == 'b') && (sibling.getColor() == 'r') && (sibling.getLeftChild().getColor() == 'b') && (sibling.getRightChild().getColor() == 'b')){
+				// Case 2
+					node.getParent().setColor('r');
+					sibling.setColor('b');
+					rightRotate(sibling); 
+					fixDelete(node); 
+				}
+				else if((node.getParent().getColor() == 'b') && (sibling.getColor() == 'b') && (sibling.getLeftChild().getColor() == 'b') && (sibling.getRightChild().getColor() == 'b')){
+				// Case 3	
+					sibling.setColor('r');
+					node.setColor('b');
+					fixDelete(node.getParent()); 
+				}
+				else if((node.getParent().getColor() == 'r') && (sibling.getColor() == 'b') && (sibling.getLeftChild().getColor() == 'b') && (sibling.getRightChild().getColor() == 'b')){
+				// Case 4
+					node.getParent().setColor('b');
+					sibling.setColor('r');
+					node.setColor('b');
+				}
+				else if((sibling.getColor() == 'b') && (sibling.getRightChild().getColor() == 'r') && (sibling.getLeftChild().getColor() == 'b')){
+				// Case 5
+					sibling.setColor('r');
+					sibling.getRightChild().setColor('b');
+					leftRotate(sibling.getRightChild());
+					fixDelete(node); 
+				}
+				else if((sibling.getColor() == 'b') && (sibling.getLeftChild().getColor() == 'r')){
+				// Case 6
+					sibling.setColor(node.getParent().getColor());
+					node.getParent().setColor('b');
+					sibling.getLeftChild().setColor('b');
+					node.setColor('b');
+					rightRotate(sibling); 
+				}
+				else{	
+				// Just in case for some reason it does not go into any cases was used for testing
+				}
+			}
+		}	
+	}
 }
 
 
--- lab4/src/Test.java
+++ lab4/src/Test.java
@@ -44,7 +44,6 @@ public class Test {
 	    				break;		
 	    		}
 	    	}
-	    	
 	    	scan.close();
 	    }
 }
